4575eebd3ce71732df71c41cddd47e9e3d7ca0fe
bright <2207244732@qq.com>
2020-12-28 11:35:21 +0800

    add mobile

---
+++ mobile/src/components/tab-bar/index.vue
@@ -0,0 +1,24 @@
+<template>
+    <div class="tab-bar">
+        <van-tabbar v-model="active">
+        <van-tabbar-item icon="home-o">
+            首页
+        </van-tabbar-item>
+        <van-tabbar-item icon="search">
+            分类
+        </van-tabbar-item>
+        <van-tabbar-item icon="friends-o">
+            购物车
+        </van-tabbar-item>
+        <van-tabbar-item icon="setting-o">
+            个人中心
+        </van-tabbar-item>
+        </van-tabbar>
+    </div>
+</template>
+
+<script>
+export default {
+    
+}
+</script>
